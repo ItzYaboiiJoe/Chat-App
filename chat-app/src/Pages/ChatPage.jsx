7c3dca791d6e4aec50173609538d4483e8befd50
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-08-15 00:03:19 -0400

    Removed Users and added the ability to send message

--- chat-app/src/Pages/ChatPage.jsx
+++ chat-app/src/Pages/ChatPage.jsx
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
-  collection,
+  doc,
   onSnapshot,
-  getDocs,
+  collection,
   query,
   where,
+  getDocs,
+  setDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { db, auth } from "../firebase";
 import SignOutConfirmation from "../Components/SignOutConfirmation";
 
 function ChatPage() {
   const [availableRooms, setAvailableRooms] = useState([]);
-  const [availableUsers, setAvailableUsers] = useState([]);
   const [selectedRoom, setSelectedRoom] = useState("");
-  const [selectedUser, setSelectedUser] = useState(null);
   const [isSignOutModalOpen, setIsSignOutModalOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("rooms");
   const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -33,6 +35,8 @@ function ChatPage() {
         if (!querySnapshot.empty) {
           const userDoc = querySnapshot.docs[0];
           setUsername(userDoc.data().username);
+        } else {
+          setUsername("Guest");
         }
       };
 
@@ -41,47 +45,62 @@ function ChatPage() {
   }, []);
 
   useEffect(() => {
-    if (selectedOption === "rooms") {
-      const roomCollectionRef = collection(db, "chatrooms");
+    const unsubscribe = onSnapshot(collection(db, "chatrooms"), (snapshot) => {
+      const rooms = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        name: doc.id,
+      }));
+      setAvailableRooms(rooms);
+    });
 
-      const unsubscribe = onSnapshot(roomCollectionRef, (querySnapshot) => {
-        const rooms = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setAvailableRooms(rooms);
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (selectedRoom) {
+      const roomDocRef = doc(db, "chatrooms", selectedRoom);
+
+      const unsubscribe = onSnapshot(roomDocRef, (docSnapshot) => {
+        if (docSnapshot.exists()) {
+          const data = docSnapshot.data();
+          const messages = Object.keys(data)
+            .filter((key) => key.startsWith("message_"))
+            .map((key) => data[key]);
+
+          setMessages(messages);
+        }
       });
 
       return () => unsubscribe();
-    } else if (selectedOption === "users") {
-      const fetchUsers = async () => {
-        const currentUser = auth.currentUser;
-
-        if (currentUser) {
-          const email = currentUser.email;
-
-          const q = query(collection(db, "users"), where("email", "!=", email));
-          const usersSnapshot = await getDocs(q);
-          const usersList = usersSnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setAvailableUsers(usersList);
-        }
-      };
-
-      fetchUsers();
     }
-  }, [selectedOption]);
+  }, [selectedRoom]);
 
   const handleRoomClick = (room) => {
     setSelectedRoom(room.id);
-    setSelectedUser(null);
+    setMessages([]);
   };
 
-  const handleUserClick = (user) => {
-    setSelectedUser(user);
-    setSelectedRoom("");
+  const handleSendMessage = async () => {
+    if (!selectedRoom || !message.trim()) return;
+
+    try {
+      const roomDocRef = doc(db, "chatrooms", selectedRoom);
+
+      const timestamp = new Date().getTime();
+      const newMessageField = `message_${timestamp}`;
+
+      await updateDoc(roomDocRef, {
+        [newMessageField]: {
+          username: username,
+          message: message,
+          timestamp: new Date(),
+        },
+      });
+
+      setMessage("");
+    } catch (error) {
+      console.error("Failed to send message: ", error.message);
+    }
   };
 
   const handleSignOutClick = () => {
@@ -116,99 +135,68 @@ function ChatPage() {
           </button>
         </div>
 
-        <div className="flex flex-col mb-8">
-          <button
-            onClick={() => setSelectedOption("rooms")}
-            className={`p-3 mb-2 rounded-lg flex items-center ${
-              selectedOption === "rooms" ? "bg-gray-700" : "bg-gray-800"
-            } hover:bg-gray-700 transition-colors`}
-          >
-            <span className="ml-2">Rooms</span>
-          </button>
-          <button
-            onClick={() => setSelectedOption("users")}
-            className={`p-3 mb-2 rounded-lg flex items-center ${
-              selectedOption === "users" ? "bg-gray-700" : "bg-gray-800"
-            } hover:bg-gray-700 transition-colors`}
-          >
-            <span className="ml-2">Users</span>
-          </button>
+        <div className="flex-grow flex flex-col">
+          <h2 className="text-xl font-semibold mb-3">Available Rooms</h2>
+          <ul className="overflow-y-auto">
+            {availableRooms.map((room) => (
+              <li
+                key={room.id}
+                className={`p-3 mb-2 rounded-lg flex justify-between items-center cursor-pointer ${
+                  room.id === selectedRoom ? "bg-gray-700" : "bg-gray-900"
+                } hover:bg-gray-700 transition-colors`}
+                onClick={() => handleRoomClick(room)}
+              >
+                <span>{room.name}</span>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
 
-        {selectedOption === "rooms" && (
-          <div className="flex-grow flex flex-col">
-            <div className="flex-grow">
-              <h2 className="text-xl font-semibold mb-3">Available Rooms</h2>
-              <ul className="overflow-y-auto">
-                {availableRooms.map((room) => (
-                  <li
-                    key={room.id}
-                    className={`p-3 mb-2 rounded-lg flex justify-between items-center cursor-pointer ${
-                      room.id === selectedRoom ? "bg-gray-700" : "bg-gray-900"
-                    } hover:bg-gray-700 transition-colors`}
-                    onClick={() => handleRoomClick(room)}
-                  >
-                    <span>{room.id}</span>
+      <div className="w-full md:w-3/4 flex flex-col h-full">
+        {selectedRoom ? (
+          <>
+            <div className="bg-gray-100 p-4 border-b">
+              <h2 className="text-xl font-semibold">{selectedRoom}</h2>
+            </div>
+            <div className="flex-grow p-4 overflow-y-auto bg-gray-50">
+              <ul>
+                {messages.map((msg) => (
+                  <li key={msg.id} className="mb-3">
+                    <div className="text-sm">
+                      <strong>{msg.username}</strong>: {msg.message}
+                    </div>
+                    <div className="text-xs text-gray-500">
+                      <em>
+                        {new Date(msg.timestamp?.toDate()).toLocaleString()}
+                      </em>
+                    </div>
                   </li>
                 ))}
               </ul>
             </div>
-          </div>
-        )}
 
-        {selectedOption === "users" && (
-          <div className="flex-grow flex flex-col">
-            <h2 className="text-xl font-semibold mb-3">Available Users</h2>
-            <ul className="flex-grow overflow-y-auto max-h-40 md:max-h-80">
-              {availableUsers.map((user) => (
-                <li
-                  key={user.id}
-                  className={`p-3 mb-2 rounded-lg flex justify-between items-center cursor-pointer bg-gray-900 hover:bg-gray-700 transition-colors`}
-                  onClick={() => handleUserClick(user)}
+            <div className="p-3 bg-gray-100 border-t flex-none">
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  placeholder="Type your message..."
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="flex-grow p-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+                />
+                <button
+                  onClick={handleSendMessage}
+                  className="ml-3 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
                 >
-                  <span>{user.username}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-
-      <div className="w-full md:w-3/4 flex flex-col">
-        {selectedRoom || selectedUser ? (
-          <>
-            <div className="bg-gray-100 p-4 border-b">
-              <h2 className="text-xl font-semibold">
-                {selectedRoom ? selectedRoom : selectedUser?.username}
-              </h2>
-            </div>
-            <div className="flex-grow p-4 overflow-y-auto bg-gray-50">
-              <p className="text-center text-gray-500">
-                Chat messages will appear here
-              </p>
-            </div>
-            <div className="p-3 bg-gray-100 border-t flex">
-              <input
-                type="text"
-                placeholder="Type your message..."
-                className="flex-grow p-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-                disabled
-              />
-              <button
-                className="ml-3 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
-                disabled
-              >
-                Send
-              </button>
+                  Send
+                </button>
+              </div>
             </div>
           </>
         ) : (
           <div className="flex items-center justify-center flex-grow text-gray-500">
-            <h2 className="text-xl">
-              {selectedOption === "rooms"
-                ? "Select a room to start chatting"
-                : "Select a user to start chatting"}
-            </h2>
+            <h2 className="text-xl">Select a room to start chatting</h2>
           </div>
         )}
       </div>
