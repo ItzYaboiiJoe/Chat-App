ac6548853a5839cb65682ee654a627c7096c7b8b
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-08-19 12:12:56 -0400

    Fixed Refresh Page Issue

--- chat-app/src/Pages/ChatPage.jsx
+++ chat-app/src/Pages/ChatPage.jsx
@@ -17,35 +17,36 @@ function ChatPage() {
   const [availableRooms, setAvailableRooms] = useState([]);
   const [selectedRoom, setSelectedRoom] = useState("");
   const [isSignOutModalOpen, setIsSignOutModalOpen] = useState(false);
-  const [username, setUsername] = useState(
-    localStorage.getItem("username") || "Guest"
-  );
+  const [username, setUsername] = useState("Guest");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!localStorage.getItem("username")) {
-      const currentUser = auth.currentUser;
+    const currentUser = auth.currentUser;
 
-      if (currentUser) {
-        const email = currentUser.email;
+    if (currentUser) {
+      const email = currentUser.email;
 
-        const fetchUsername = async () => {
-          const q = query(collection(db, "users"), where("email", "==", email));
-          const querySnapshot = await getDocs(q);
+      const fetchUsername = async () => {
+        const q = query(collection(db, "users"), where("email", "==", email));
+        const querySnapshot = await getDocs(q);
 
-          if (!querySnapshot.empty) {
-            const userDoc = querySnapshot.docs[0];
-            const fetchedUsername = userDoc.data().username;
-            setUsername(fetchedUsername);
-            localStorage.setItem("username", fetchedUsername);
-          } else {
-            setUsername("Guest");
-          }
-        };
+        if (!querySnapshot.empty) {
+          const userDoc = querySnapshot.docs[0];
+          const fetchedUsername = userDoc.data().username;
+          setUsername(fetchedUsername);
+          localStorage.setItem("username", fetchedUsername);
+        } else {
+          setUsername("Guest");
+        }
+      };
 
-        fetchUsername();
+      fetchUsername();
+    } else {
+      const storedUsername = localStorage.getItem("username");
+      if (storedUsername) {
+        setUsername(storedUsername);
       }
     }
   }, []);
@@ -71,8 +72,7 @@ function ChatPage() {
           const data = docSnapshot.data();
           const messages = Object.keys(data)
             .filter((key) => key.startsWith("message_"))
-            .map((key) => data[key])
-            .sort((a, b) => a.timestamp.seconds - b.timestamp.seconds);
+            .map((key) => data[key]);
 
           setMessages(messages);
         }
@@ -135,94 +135,97 @@ function ChatPage() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row h-screen">
-      <div className="w-full md:w-1/4 bg-gray-800 text-white p-3 flex flex-col">
-        <div className="flex items-center justify-between mb-8">
-          <div>
-            <h3 className="text-lg font-semibold">Welcome, {username}!</h3>
+    <>
+      <SessionManager />
+      <div className="flex flex-col md:flex-row h-screen">
+        <div className="w-full md:w-1/4 bg-gray-800 text-white p-3 flex flex-col">
+          <div className="flex items-center justify-between mb-8">
+            <div>
+              <h3 className="text-lg font-semibold">Welcome, {username}!</h3>
+            </div>
+            <button
+              onClick={handleSignOutClick}
+              className="bg-red-500 text-white px-3 py-1 rounded-lg hover:bg-red-600 transition-colors"
+            >
+              Sign Out
+            </button>
           </div>
-          <button
-            onClick={handleSignOutClick}
-            className="bg-red-500 text-white px-3 py-1 rounded-lg hover:bg-red-600 transition-colors"
-          >
-            Sign Out
-          </button>
-        </div>
 
-        <div className="flex-grow flex flex-col">
-          <h2 className="text-xl font-semibold mb-3">Available Rooms</h2>
-          <ul className="overflow-y-auto">
-            {availableRooms.map((room) => (
-              <li
-                key={room.id}
-                className={`p-3 mb-2 rounded-lg flex justify-between items-center cursor-pointer ${
-                  room.id === selectedRoom ? "bg-gray-700" : "bg-gray-900"
-                } hover:bg-gray-700 transition-colors`}
-                onClick={() => handleRoomClick(room)}
-              >
-                <span>{room.name}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      <div className="w-full md:w-3/4 flex flex-col h-full">
-        {selectedRoom ? (
-          <>
-            <div className="bg-gray-100 p-4 border-b">
-              <h2 className="text-xl font-semibold">{selectedRoom}</h2>
-            </div>
-            <div className="flex-grow p-4 overflow-y-auto bg-gray-50">
-              <ul>
-                {messages.map((msg, index) => (
-                  <li key={index} className="mb-3">
-                    <div className="text-sm">
-                      <strong>{msg.username}</strong>: {msg.message}
-                    </div>
-                    <div className="text-xs text-gray-500">
-                      <em>
-                        {new Date(msg.timestamp?.toDate()).toLocaleString()}
-                      </em>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="p-3 bg-gray-100 border-t flex-none">
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  placeholder="Type your message..."
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  onKeyDown={handleKeyPress}
-                  className="flex-grow p-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-                />
-                <button
-                  onClick={handleSendMessage}
-                  className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
+          <div className="flex-grow flex flex-col">
+            <h2 className="text-xl font-semibold mb-3">Available Rooms</h2>
+            <ul className="overflow-y-auto">
+              {availableRooms.map((room) => (
+                <li
+                  key={room.id}
+                  className={`p-3 mb-2 rounded-lg flex justify-between items-center cursor-pointer ${
+                    room.id === selectedRoom ? "bg-gray-700" : "bg-gray-900"
+                  } hover:bg-gray-700 transition-colors`}
+                  onClick={() => handleRoomClick(room)}
                 >
-                  Send
-                </button>
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className="flex items-center justify-center flex-grow text-gray-500">
-            <h2 className="text-xl">Select a room to start chatting</h2>
+                  <span>{room.name}</span>
+                </li>
+              ))}
+            </ul>
           </div>
-        )}
-      </div>
+        </div>
 
-      <SignOutConfirmation
-        isOpen={isSignOutModalOpen}
-        onClose={handleCloseSignOutModal}
-        onConfirm={handleSignOutConfirm}
-        message="Are you sure you want to sign out?"
-      />
-    </div>
+        <div className="w-full md:w-3/4 flex flex-col h-full">
+          {selectedRoom ? (
+            <>
+              <div className="bg-gray-100 p-4 border-b">
+                <h2 className="text-xl font-semibold">{selectedRoom}</h2>
+              </div>
+              <div className="flex-grow p-4 overflow-y-auto bg-gray-50">
+                <ul>
+                  {messages.map((msg, index) => (
+                    <li key={index} className="mb-3">
+                      <div className="text-sm">
+                        <strong>{msg.username}</strong>: {msg.message}
+                      </div>
+                      <div className="text-xs text-gray-500">
+                        <em>
+                          {new Date(msg.timestamp?.toDate()).toLocaleString()}
+                        </em>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="p-3 bg-gray-100 border-t flex-none">
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    placeholder="Type your message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                    className="flex-grow p-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+                  />
+                  <button
+                    onClick={handleSendMessage}
+                    className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
+                  >
+                    Send
+                  </button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="flex items-center justify-center flex-grow text-gray-500">
+              <h2 className="text-xl">Select a room to start chatting</h2>
+            </div>
+          )}
+        </div>
+
+        <SignOutConfirmation
+          isOpen={isSignOutModalOpen}
+          onClose={handleCloseSignOutModal}
+          onConfirm={handleSignOutConfirm}
+          message="Are you sure you want to sign out?"
+        />
+      </div>
+    </>
   );
 }
 
